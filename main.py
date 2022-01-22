dd0bb4256c239a5b9921d0a8e4c514d6537a715b
Jakub Baran <b.kuba.13@gmail.com>
2022-01-22 15:46:21 +0100

    add new theme

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from __future__ import unicode_literals
 import youtube_dl
-import tkinter as tk
 from tkinter import filedialog as fd
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
 
 def download(link, is_mp3):
     path = output_dir_entry.get()
@@ -22,31 +23,36 @@ def download(link, is_mp3):
         ydl.download([link])
 
 
-root = tk.Tk()
+root = ttk.Window(themename='darkly')
 
-title_label = tk.Label(root, text="YouTube mp3/mp4 dowlander")
+title_label = ttk.Label(root, text="YouTube mp3/mp4 dowlander")
 
-insert_link_frame = tk.LabelFrame(root, text="Insert YouTube link")
-insert_link_entry = tk.Entry(insert_link_frame, width=60)
+insert_link_frame = ttk.LabelFrame(root, text="Insert YouTube link")
+insert_link_entry = ttk.Entry(insert_link_frame, width=60)
 
-file_format_frame = tk.Frame(root)
-is_mp3 = tk.BooleanVar()
-mp3_radio = tk.Radiobutton(file_format_frame, text="mp3", variable=is_mp3, value=True)
-mp4_radio = tk.Radiobutton(file_format_frame, text="mp4", variable=is_mp3, value=False)
+file_format_frame = ttk.Frame(root)
+is_mp3 = ttk.BooleanVar()
+mp3_radio = ttk.Radiobutton(file_format_frame, text="mp3", variable=is_mp3, value=True)
+mp4_radio = ttk.Radiobutton(file_format_frame, text="mp4", variable=is_mp3, value=False)
 
-output_dir_frame = tk.LabelFrame(root, text='Select path to save download files')
-output_dir_entry = tk.Entry(output_dir_frame, width=52)
-browser_button = tk.Button(output_dir_frame, text='Browse', command=lambda : output_dir_entry.insert(0, fd.askdirectory()))
-download_button = tk.Button(root, text="Download", command= lambda : download(insert_link_entry.get(),
-                                                                              is_mp3.get()))
+output_dir_frame = ttk.LabelFrame(root, text='Select path to save download files')
+output_dir_entry = ttk.Entry(output_dir_frame, width=46)
+browser_button = ttk.Button(output_dir_frame,
+                            text='Browse',
+                            command=lambda : output_dir_entry.insert(0, fd.askdirectory()),
+                            bootstyle=PRIMARY)
+download_button = ttk.Button(root,
+                             text="Download",
+                             command= lambda : download(insert_link_entry.get(), is_mp3.get()),
+                             bootstyle=DANGER)
 title_label.pack(pady=20)
 insert_link_frame.pack(padx=20, pady=20)
 insert_link_entry.pack(padx=20, pady=15)
 output_dir_frame.pack()
-output_dir_entry.grid(row=0, column=0, padx=10, pady=10)
+output_dir_entry.grid(row=0, column=0, padx=20, pady=10)
 browser_button.grid(row=0, column=1, padx=10, pady=10)
-file_format_frame.pack(padx=20, pady=10)
-mp3_radio.grid(row=0, column=0)
-mp4_radio.grid(row=0, column=1)
-download_button.pack(pady=25)
+file_format_frame.pack(padx=20, pady=20)
+mp3_radio.grid(row=0, column=0, padx=10)
+mp4_radio.grid(row=0, column=1, padx=10)
+download_button.pack(pady=15)
 root.mainloop()
